1e0f68894e0d264631b6eba03f5e50d1eea10e97
Samuel Freeman <samuelmanueljnr@gmail.com>
2023-12-23 15:54:32 +0000

    More server cleaning

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const { PrismaClient } = require('@prisma/client');
+const bodyParser = require("body-parser");
 
-const bodyParser = require('body-parser');
+const apiRoute = require("./routes/index");
 
 const PORT = 8080;
-const authorRoutes = require('./routes/authors');
 
-const albumRoutes = require('./routes/albums');
-
-const songRoutes = require('./routes/songs');
-
-const prisma = new PrismaClient();
-
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors({ origin: true, credentials: true }));
 
 app.use(bodyParser.json());
-// author routes
-app.use(authorRoutes);
-// album routes
-app.use(albumRoutes);
-//song routes
-app.use(songRoutes);
-
-
+app.use(bodyParser.urlencoded());
+app.use("api", apiRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
