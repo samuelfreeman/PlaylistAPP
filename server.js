8dcaf22d860483f7614f536f4f84661d717921ce
Samuel Freeman <samuelmanueljnr@gmail.com>
2023-12-23 15:41:21 +0000

    cleaned the server

--- server.js
+++ server.js
@@ -26,69 +26,8 @@ app.use(authorRoutes);
 app.use(albumRoutes);
 //song routes
 app.use(songRoutes);
-app.patch('/album/:id', async (req, res, next) => {
-  const id = parseInt(req.params.id);
-  const data = req.body;
-  const albums = await prisma.albums.update({
-    where: {
-      id,
-    },
 
-    data,
-  });
-  res.status(200).json({
-    albums,
-  });
-});
-app.get('/albums', async (req, res, next) => {
-  const albums = await prisma.albums.findMany({
-    include: {
-      author: {
-        include: {
-          albums: {
-            include: {
-              songs: true,
-            },
-          },
-        },
-      },
-    },
-  });
-  res.status(200).json({
-    albums,
-  });
-});
 
-app.delete('/album/:id', async (req, res, next) => {
-  const id = parseInt(req.params.id);
-  const albums = await prisma.albums.findMany({
-    where: {
-      id,
-    },
-    include: {
-      albums: {
-        include: {
-          songs: true,
-        },
-      },
-    },
-  });
-  res.status(200).json({
-    albums,
-  });
-});
-
-// //songs
-app.post('/songs', async (req, res, next) => {
-  const data = req.body;
-  const song = await prisma.songs.create({
-    data,
-  });
-
-  res.status(201).json({
-    song,
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
